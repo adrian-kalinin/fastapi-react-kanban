50e25ad671985c3fc2852a31055209ea61609114
Adrian Kalinin <adriankalinin@protonmail.com>
2022-07-10 00:10:32 +0300

    Creat endpoint /board with dummy data

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/board")
+def board():
+    board_data = {
+        "tasks": {
+            "task-1": {"id": "task-1", "content": "Create video"},
+            "task-2": {"id": "task-2", "content": "Edit video"},
+            "task-3": {"id": "task-3", "content": "Publish video"},
+        },
+        "columns": {
+            "column-1": {
+                "id": "column-1",
+                "title": "To Do",
+                "tasks": ["task-2", "task-3"],
+            },
+            "column-2": {
+                "id": "column-2",
+                "title": "Done",
+                "tasks": ["task-1"],
+            },
+        },
+        "columnOrder": ["column-1", "column-2"],
+    }
+
+    return board_data
