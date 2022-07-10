98f6cd69f03727a72b6e1d8842c46de2c717464a
Adrian Kalinin <adriankalinin@protonmail.com>
2022-07-10 15:01:03 +0300

    Create directory for components

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Board from './Board';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Board />
     </div>
   );
 }
---
+++ frontend/src/components/Board.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+  display: flex;
+`;
+
+function Board(props) {
+  return (
+    <Container>
+      Board
+    </Container>
+  );
+}
+
+export default Board;
