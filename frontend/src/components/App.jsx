b60a72db13852160f8f5bdf9c048cc0d4c811255
Adrian Kalinin <adriankalinin@protonmail.com>
2022-07-28 10:47:33 +0200

    Add navbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Board from './Board';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <hr />
       <Board />
     </div>
   );
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Navbar(props) {
+
+  return (
+    <nav className="container flex justify-between py-4 mx-auto bg-white">
+      <div className="flex items-center">
+        <h3 className="text-2xl font-medium text-blue-500">Kanban</h3>
+      </div>
+      <div className="flex items-center">
+        <button type="button" className="bg-gray-100 rounded px-5 py-3 border border-gray text-gray-700 font-semibold tracking-wide text-sm">
+          Login
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
