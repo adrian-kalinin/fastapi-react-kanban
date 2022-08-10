3e8d689fe6839009fd855c1ff9ce3e7ccf2427b4
Adrian Kalinin <adriankalinin@protonmail.com>
2022-08-10 20:18:39 +0300

    Add routing to React app

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Board from "./Board";
 import Navbar from "./Navbar";
+import Register from "./Register";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <hr />
-      <Board />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Board />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
