d91b30d7a3e69855d73f01223648f111f2e615ab
Adrian Kalinin <adriankalinin@protonmail.com>
2022-08-10 20:17:38 +0300

    Add "Sign In" and "Sign Up" buttons to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
   return (
-    <nav className="container flex justify-between py-4 mx-auto bg-white">
-      <div className="flex items-center">
-        <h3 className="text-2xl font-medium text-blue-500">Kanban</h3>
-      </div>
-      <div className="flex items-center">
-        <button
-          type="button"
-          className="bg-gray-100 rounded px-5 py-3 border border-gray text-gray-700 font-semibold tracking-wide text-sm"
-        >
-          Login
-        </button>
+    <nav className="relative bg-white border-b-2 border-gray-100">
+      <div className="container mx-auto flex justify-between py-3 px-2">
+        <div className="flex items-center">
+          <Link to="/">
+            <h4 className="text-2xl font-bold text-indigo-600">Kanban</h4>
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <Link
+            to="/login"
+            className="whitespace-nowrap text-base text-gray-500 hover:text-gray-900"
+          >
+            Sign in
+          </Link>
+          <Link
+            to="/register"
+            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Sign up
+          </Link>
+        </div>
       </div>
     </nav>
   );
