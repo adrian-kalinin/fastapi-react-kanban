e7fd53905b64ab9fd10a48114c4f2eb08f47bf93
Adrian Kalinin <adriankalinin@protonmail.com>
2022-08-13 23:36:46 +0300

    Add extra links and create handleSubmit() function

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register(props) {
   return (
@@ -6,7 +7,7 @@ function Register(props) {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            Log in to your account
           </h2>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
@@ -40,18 +41,17 @@ function Register(props) {
               />
             </div>
           </div>
-
           <div className="flex items-center justify-between">
-            <div className="text-sm">
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
+            <div className="text-sm font-medium">
+              Don't have an account?{" "}
+              <Link
+                to="/signup"
+                className="text-indigo-600 hover:text-indigo-500"
               >
-                Forgot your password?
-              </a>
+                Sign up
+              </Link>
             </div>
           </div>
-
           <div>
             <button
               type="submit"
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,31 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  function handleSubmit() {
+    // TODO
+  }
+
+  async function createUser() {
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    const response = await fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    return await response.json();
+  }
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -9,7 +34,12 @@ function Register(props) {
             Create a new account
           </h2>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form
+          className="mt-8 space-y-6"
+          action="#"
+          method="POST"
+          onSubmit={handleSubmit}
+        >
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
               <label htmlFor="email-address" className="sr-only">
@@ -23,6 +53,7 @@ function Register(props) {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
+                onChange={(event) => setEmail(event.target.value)}
               />
             </div>
             <div>
@@ -37,10 +68,21 @@ function Register(props) {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
               />
             </div>
           </div>
-
+          <div className="flex items-center justify-between">
+            <div className="text-sm font-medium">
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="text-indigo-600 hover:text-indigo-500"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
           <div>
             <button
               type="submit"
