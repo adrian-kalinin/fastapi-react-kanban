2554ab764a08bdb2be28b2f5afa296e3369208ec
Adrian Kalinin <adriankalinin@protonmail.com>
2022-08-19 00:05:22 +0300

    Change navbar for logged in users

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="relative bg-white border-b-2 border-gray-100">
       <div className="container mx-auto flex justify-between py-3 px-2">
@@ -10,20 +10,38 @@ function Navbar() {
             <h4 className="text-2xl font-bold text-indigo-600">Kanban</h4>
           </Link>
         </div>
-        <div className="flex items-center">
-          <Link
-            to="/login"
-            className="whitespace-nowrap text-base text-gray-500 hover:text-gray-900"
-          >
-            Log in
-          </Link>
-          <Link
-            to="/signup"
-            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base text-white bg-indigo-600 hover:bg-indigo-700"
-          >
-            Sign up
-          </Link>
-        </div>
+        {!props.token ? (
+          <div className="flex items-center">
+            <Link
+              to="/login"
+              className="whitespace-nowrap text-base text-gray-500 hover:text-gray-900"
+            >
+              Log in
+            </Link>
+            <Link
+              to="/signup"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
+            </Link>
+          </div>
+        ) : (
+          <div className="flex items-center">
+            <Link
+              to="/board"
+              className="whitespace-nowrap text-base text-gray-700 hover:text-gray-900"
+            >
+              My board
+            </Link>
+            <span className="h-6 w-px mx-6 bg-gray-200" aria-hidden="true" />
+            <a
+              href="#"
+              className="whitespace-nowrap text-base text-gray-500 hover:text-gray-900"
+            >
+              Log out
+            </a>
+          </div>
+        )}
       </div>
     </nav>
   );
